9a66d954759e5af3fa0c5a53e0fef522669c9f48
rodrigo <rodrigodavila_00098@hotmail.com>
2021-02-08 01:43:31 -0300

    cambios en travelocity tests

--- src/main/java/com/automation/exam/tests/TravelocityTests.java
+++ src/main/java/com/automation/exam/tests/TravelocityTests.java
@@ -13,17 +13,19 @@ import com.automation.exam.tests.BaseTests;
 
 public class TravelocityTests extends BaseTests {
 
+	/*
 	@Test(groups = { "results" })
 	@Parameters({ "resultsUrl" })
 	public void testResults(String resultsUrl) {
 		// el url de parametro tira error en la suite.xml
 		TravelocityResults results = getTravelocityResults(resultsUrl);
 		testResults(results);
-	}
+	}*/
 
 	@Test(groups = { "search" })
 	public void testFlightBooking() {
-
+		SoftAssert softAssert = new SoftAssert();
+			
 		TravelocityHome home = getTravelocityHome();
 
 		home.goToFlightsTab();
@@ -38,25 +40,18 @@ public class TravelocityTests extends BaseTests {
 		TravelocityResults results;
 		results = home.searchFlight();
 		System.out.println("showing flight results");
-
-		testResults(results);
-	}
-
-	private void testResults(TravelocityResults results) {
-
-		SoftAssert softAssert = new SoftAssert();
-		
+	
 		//results.printDetails = true;		
 		softAssert.assertEquals(results.verifySortingBoxClickable(), true);
 		
-		System.out.println("sorting list");		
+		//System.out.println("sorting list");		
 		results.sortByShorterDuration();
 		
 		softAssert.assertEquals(results.verifySortingByShorterDuration(), true);
 		softAssert.assertEquals(results.verifySelectButtons(), true);
 		softAssert.assertEquals(results.verifyPriceTag(), true);
 		softAssert.assertEquals(results.verifyFlightDuration(), true);
-
+		
 		TravelocityResults secondResultsPage = results.selectFirstFlight(1);
 		System.out.println("first flight selected");
 
@@ -65,20 +60,19 @@ public class TravelocityTests extends BaseTests {
 		System.out.println("second flight selected");
 
 		// flightInfoPage.printDetails=true;
-		// flight info verifications
 		softAssert.assertEquals(flightInfoPage.verifyTotalPrice(), true);		
 		softAssert.assertEquals(flightInfoPage.verifyDepartureAndReturn(), true);				
 		softAssert.assertEquals(flightInfoPage.verifyGuaranteeText(), true);		
 		TravelocityWhosTraveling whosTravelingPage= flightInfoPage.clickCheckOutButton();
 		System.out.println("Check out button clicked!");
-
-		// whos traveling page verifications
+		
 		softAssert.assertEquals(whosTravelingPage.verifyFirstNameInput(), true);
 		softAssert.assertEquals(whosTravelingPage.verifyLastname(), true);
 		softAssert.assertEquals(whosTravelingPage.verifyMiddleNameInput(), true);
 		softAssert.assertEquals(whosTravelingPage.verifyPhoneInput(), true);
-		softAssert.assertEquals(whosTravelingPage.verifyPhoneCodeSelect(), true);		
-		
+		softAssert.assertEquals(whosTravelingPage.verifyPhoneCodeSelect(), true);
+				
+		softAssert.assertAll();
 	}
 
 }
